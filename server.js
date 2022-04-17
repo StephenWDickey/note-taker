f9913ee95c307eae78fa35eb8cd2bb49f7b671cf
stevey d <stphndcky22@gmail.com>
2022-04-17 06:52:03 -0700

    created lib folder, created routes folder

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // import the express module
 const express = require('express');
+// to give each note a uniqe id we can use the uuid module!
+
+
+////////////////////////////////////////////////////
+
 
 // we define our PORT as a variable one for heroku or our local one 3001
 const PORT = process.env.PORT || 3001;
@@ -7,14 +12,25 @@ const PORT = process.env.PORT || 3001;
 // we put express function in a variable so we can chain methods to it!
 const app = express();
 
+
+//////////////////////////////////////////////////////
+
+
 // we create expressions for middleware
 // this one tells us we may receive arrays within arrays
 app.use(express.urlencoded({extended: true}));
 // this one will parse json data and put it in req.body object
 app.use(express.json());
+// will use our front end files in the public folder
 app.use(express.static('public'));
 
 
+
+//////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////
 
 
 // this will start our express erver at the specified port
